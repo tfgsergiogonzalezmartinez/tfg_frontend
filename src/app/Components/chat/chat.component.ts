e711551845284dd5957859a01a3d1974b127cba2
sergio <sergio.gonzalez@tchlabs.eu>
2024-06-29 06:11:44 +0200

    Implementacion de Actulizacion de scroll del chat, mostrando los ultimos datos.

--- src/app/Components/chat/chat.component.ts
+++ src/app/Components/chat/chat.component.ts
@@ -13,11 +13,12 @@ import { ChatService } from '../../../../Services/Chat/Chat.service';
   styleUrls: ['./chat.component.css']
 })
 export class ChatComponent implements OnInit, OnDestroy {
-  @ViewChild('buscador') BuscadorComponent! : BuscadorUsuariosComponent;
+  @ViewChild('buscador') buscadorComponent!: BuscadorUsuariosComponent;
+  @ViewChild('chat') chatHtml!: ElementRef;
   isConnectionEstablished: boolean = false;
 
-  private listaUsuariosBuscados : ChatUsuariosBuscados[] = [];
-  mostrarBuscador : boolean = false;
+  private listaUsuariosBuscados: ChatUsuariosBuscados[] = [];
+  mostrarBuscador: boolean = false;
   UserBuscar: string = "";
   UsuariosBuscados: UserGetDto[] = [];
   listaUsuariosAbiertosChats: ChatUsuariosBuscados[] = [];
@@ -28,24 +29,20 @@ export class ChatComponent implements OnInit, OnDestroy {
   listaConversacion: ChatMessage[] = [];
   currentUser!: UserGetDto;
   currentImage: string = "";
-  otroUsuarioChat!: ChatUsuariosBuscados;
+  otroUsuarioChat!: ChatUsuariosBuscados | null;
   inputMensaje: string = "";
 
   isConexionInciada = false;
   private connection: HubConnection;
 
-  constructor(private userService : UserService, private chatService : ChatService, private cdr : ChangeDetectorRef) {
+  constructor(private userService: UserService, private chatService: ChatService, private cdr: ChangeDetectorRef) {
     this.connection = new HubConnectionBuilder()
       .withUrl(`http://localhost:5059/WebChat?user=${sessionStorage.getItem('Id')}`, {
         withCredentials: true
       })
       .build();
 
-      this.connection.on("NewUser", message => this.newUser(message));
-      this.connection.on("NewMessage", (message: ChatMessage, group: string) => this.newMessage(message, group));
-      this.connection.on("LeftUser", message => this.leftUser(message));
-      this.connection.on("JoinGroup", (group: string) => this.handleJoinGroup(group));
-      this.connection.on("mensajePrivado", (message: ChatMessage) => this.recibirMensaje_privado(message));
+    this.connection.on("mensajePrivado", (message: ChatMessage) => this.recibirMensaje_privado(message));
 
   }
 
@@ -71,11 +68,11 @@ export class ChatComponent implements OnInit, OnDestroy {
 
     this.chatService.GetChatsAbiertos(sessionStorage.getItem('Id')!).subscribe({
       next: data => {
-        for (const chat of data){
-          for (const user of chat.UserIds){
-            if (user != sessionStorage.getItem('Id')){  //si es distinto al usuario actual, cargamos su foto y sus mensajes no leidos
+        for (const chat of data) {
+          for (const user of chat.UserIds) {
+            if (user != sessionStorage.getItem('Id')) {  //si es distinto al usuario actual, cargamos su foto y sus mensajes no leidos
               this.userService.GetById(user).subscribe({
-                next: data =>{
+                next: data => {
                   this.cargarFoto(data, user);
                 },
                 error: error => {
@@ -86,8 +83,8 @@ export class ChatComponent implements OnInit, OnDestroy {
           }
         }
       },
-      error : error => {
-
+      error: error => {
+        console.error("Error al obtener los chats abiertos", error);
       }
     });
   }
@@ -100,8 +97,9 @@ export class ChatComponent implements OnInit, OnDestroy {
 
   private recibirMensaje_privado(message: ChatMessage) {
     console.log('Mensaje directo recibido:', message);
+    this.scrollHastaAbajo();
     this.actualizarMensajesNoLeidos(message.usuario); //Actualizo los mensajes no leidos a partir de la primera vez.
-    if (this.otroUsuarioChat && this.otroUsuarioChat.User.Id == message.usuario){
+    if (this.otroUsuarioChat && this.otroUsuarioChat.User.Id == message.usuario) {
       this.chatService.LeerChat(message.usuario, sessionStorage.getItem('Id')!).subscribe({
         next: data => {
           this.actualizarMensajesNoLeidos(message.usuario); //Actualizo los mensajes a 0 en caso de que esten leidos
@@ -113,7 +111,7 @@ export class ChatComponent implements OnInit, OnDestroy {
       });
     }
     this.userService.GetById(message.usuario).subscribe({
-      next: data =>{
+      next: data => {
         this.cargarFoto(data, message.usuario);
       },
       error: error => {
@@ -122,10 +120,6 @@ export class ChatComponent implements OnInit, OnDestroy {
     });
     this.listaConversacion.push(message);
 
-    this.cdr.detectChanges();
-  }
-
-  private handleJoinGroup(group: string) {
   }
 
   public join(grupo: string) {
@@ -134,45 +128,13 @@ export class ChatComponent implements OnInit, OnDestroy {
       .catch(err => console.error('Join Group Error: ', err));
   }
 
-  public sendMessage() {
-    const newMessage: ChatMessage = {
-      mensaje: this.message,
-      usuario: this.user,
-    };
-
-    this.connection.invoke('SendMessage', newMessage)
-      .then(() => this.message = '')
-      .catch(err => console.error('Send Message Error: ', err));
-  }
-
-  public leave() {
-    this.connection.invoke('LeaveGroup', this.user)
-      .then(() => this.connected = false)
-      .catch(err => console.error('Leave Group Error: ', err));
-  }
-
-  private newUser(message: string) {
-    console.log(message);
-    this.listaConversacion.push({ usuario: 'Sistema', mensaje: message });
-  }
-
-  private newMessage(message: ChatMessage, group: string) {
-    console.log(message);
-    this.cdr.detectChanges();  // Asegurar que los cambios se detecten
-  }
-
-
-  private leftUser(message: string) {
-    console.log();
-    this.listaConversacion.push({ usuario: 'Sistema', mensaje: message });
-  }
-
-  actualizarMensajesNoLeidos(idUsuario : string){
-    for(const user of this.listaUsuariosAbiertosChats){
-      if (user.User.Id == idUsuario){
+  actualizarMensajesNoLeidos(idUsuario: string) {
+    for (const user of this.listaUsuariosAbiertosChats) {
+      if (user.User.Id == idUsuario) {
         this.chatService.GetNumMensajesSinLeer(idUsuario, sessionStorage.getItem('Id')!).subscribe({
           next: data => {
             user.MensajesNoLeidos = data.MensajesNoLeidos;
+            this.scrollHastaAbajo();
           }
         });
         break;
@@ -180,20 +142,20 @@ export class ChatComponent implements OnInit, OnDestroy {
     }
   }
 
-  getListUsuariosBuscados(){
+  getListUsuariosBuscados() {
     return this.listaUsuariosBuscados;
   }
 
   //!ASIGNO LA IMAGEN AQUI YA QUE SON METODOS ASYNCRONOS Y PUEDE QUE NO COMPLEMETEN TOTALMENTE SU EJECUCION y DEVUELVA LOS DATOS ANTES DE ASIGNARLOS.
-  cargarFoto(user : UserGetDto, id : string) {
+  cargarFoto(user: UserGetDto, id: string) {
     this.userService.getFotoAvatar(id).subscribe({
       next: data => {
-        if (this.listaUsuariosAbiertosChats.length > 0){
-          for (const user of this.listaUsuariosAbiertosChats){
+        if (this.listaUsuariosAbiertosChats.length > 0) {
+          for (const user of this.listaUsuariosAbiertosChats) {
             if (user.User.Id == id) return;
           }
         }
-        const chatUsuario : ChatUsuariosBuscados = {User: user , Imagen: data.Imagen, MensajesNoLeidos: 0};
+        const chatUsuario: ChatUsuariosBuscados = { User: user, Imagen: data.Imagen, MensajesNoLeidos: 0 };
         this.abrirChat(chatUsuario);
         console.log(data);
       },
@@ -204,18 +166,20 @@ export class ChatComponent implements OnInit, OnDestroy {
   }
 
 
-  addUsuarioChat(user : ChatUsuariosBuscados){
+  addUsuarioChat(user: ChatUsuariosBuscados) {
+    this.cargarMensajesBBDD(user);
     if (this.listaUsuariosAbiertosChats.length <= 0) this.abrirChat(user);
-    for( const userLista of this.listaUsuariosAbiertosChats){
+    for (const userLista of this.listaUsuariosAbiertosChats) {
       if (user.User.Id == userLista.User.Id) break;
       this.abrirChat(user);
     }
+    this.scrollHastaAbajo();
     this.otroUsuarioChat = user;
-    this.BuscadorComponent.mostrarBuscador = false;
+    this.buscadorComponent.mostrarBuscador = false;
     this.isConexionInciada = false; //Reinicio la conexion para que se vuelva a conectar con el nuevo chat tras el primer mensaje
   }
 
-  setChatActivo(user : ChatUsuariosBuscados){
+  setChatActivo(user: ChatUsuariosBuscados) {
     this.cargarMensajesBBDD(user);
     this.otroUsuarioChat = user;
     this.chatService.LeerChat(user.User.Id, sessionStorage.getItem('Id')!).subscribe({
@@ -226,11 +190,12 @@ export class ChatComponent implements OnInit, OnDestroy {
         console.error("Error al descargar la foto", error);
       }
     });
-    this.BuscadorComponent.mostrarBuscador = false;
+    this.scrollHastaAbajo();
+    this.buscadorComponent.mostrarBuscador = false;
     this.isConexionInciada = false; //Reinicio la conexion para que se vuelva a conectar con el nuevo chat tras el primer mensaje
   }
 
-  getCurrentID(){
+  getCurrentID() {
     return sessionStorage.getItem('Id');
   }
 
@@ -238,39 +203,40 @@ export class ChatComponent implements OnInit, OnDestroy {
     const newMessage: ChatMessage = {
       mensaje: this.inputMensaje,
       usuario: sessionStorage.getItem('Id')!,
-      grupo: sessionStorage.getItem('Id') + "$" + this.otroUsuarioChat.User.Id,
-      destinatario: this.otroUsuarioChat.User.Id // Asegúrate de tener el destinatario en el mensaje
+      grupo: sessionStorage.getItem('Id') + "$" + this.otroUsuarioChat!.User.Id,
+      destinatario: this.otroUsuarioChat!.User.Id // Asegúrate de tener el destinatario en el mensaje
     };
 
-    if (!this.isConexionInciada) {
-      this.join(newMessage.grupo!)!;
-      this.isConexionInciada = true;
-    }
 
-    this.connection.invoke('SendMessage', newMessage)
+    this.connection.invoke('onEnviarMensajeDirecto', newMessage)
       .then(() => {
         this.listaConversacion.push(newMessage);
         this.inputMensaje = '';
+        this.scrollHastaAbajo();
       })
       .catch(err => console.error('Send Message Error: ', err));
   }
 
-  cerrarChat(user : ChatUsuariosBuscados ){
+  cerrarChat(user: ChatUsuariosBuscados) {
     this.chatService.CerrarChat(sessionStorage.getItem('Id')!, user.User.Id).subscribe({
-      next: data =>{
+      next: data => {
         this.listaUsuariosAbiertosChats = this.listaUsuariosAbiertosChats.filter(x => x.User.Id != user.User.Id);
+        this.listaConversacion = [];
+        this.otroUsuarioChat = null;
       }
     });
   }
 
-  cargarMensajesBBDD(user : ChatUsuariosBuscados){
+  cargarMensajesBBDD(user: ChatUsuariosBuscados) {
     this.listaConversacion = [];
     this.chatService.GetByUsers(sessionStorage.getItem("Id")!, user.User.Id).subscribe({
       next: data => {
-        for (const message of data.Mensajes){
-          const msg : ChatMessage = {mensaje: message.Msg, usuario: message.UserId};
+        for (const message of data.Mensajes) {
+          const msg: ChatMessage = { mensaje: message.Msg, usuario: message.UserId };
           this.listaConversacion.push(msg);
+          // this.scrollHastaAbajo();
         }
+        this.scrollHastaAbajo();
       },
       error: error => {
         console.error("Error al descargar la foto", error);
@@ -279,9 +245,9 @@ export class ChatComponent implements OnInit, OnDestroy {
   }
 
 
-  abrirChat(user : ChatUsuariosBuscados){
+  abrirChat(user: ChatUsuariosBuscados) {
     this.chatService.AbrirChat(sessionStorage.getItem('Id')!, user.User.Id).subscribe({
-      next: data =>{
+      next: data => {
         this.listaUsuariosAbiertosChats.push(user);
         this.actualizarMensajesNoLeidos(user.User.Id);
       },
@@ -289,6 +255,11 @@ export class ChatComponent implements OnInit, OnDestroy {
         console.error("Error al descargar la foto", error);
       }
     });
+  }
+
+  scrollHastaAbajo() {
+    this.cdr.detectChanges();
+    this.chatHtml.nativeElement.scrollTop = this.chatHtml.nativeElement.scrollHeight;
   }
 }
 
