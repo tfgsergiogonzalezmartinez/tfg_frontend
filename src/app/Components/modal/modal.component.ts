4b65f43873a457052be101e906e15b1a296e2544
sergio <sergio.gonzalez@tchlabs.eu>
2024-07-02 11:27:10 +0200

    Solucion de bugs varios

--- src/app/Components/modal/modal.component.ts
+++ src/app/Components/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, Output, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-modal',
@@ -6,17 +6,33 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
   styleUrls: ['./modal.component.css']
 })
 export class ModalComponent implements OnInit {
+  @ViewChild('modal') modalRef! : ElementRef;
   @Output() exitEventEmitter : EventEmitter<void> = new EventEmitter<void>();
-  constructor() { }
+  private clickFondoListener!: () => void;
+  private primeraVez : boolean = true;
+  constructor(private renderer2 : Renderer2) { }
 
   ngOnInit() {
-
-
-
+  }
+  ngAfterViewInit(): void {
+    this.clickFondoListener = this.renderer2.listen('document', 'click', (event) => {
+      this.clickFuera(event);
+    });
   }
 
   closeModal(){
     this.exitEventEmitter.emit();
+  }
+
+  clickFuera(event: any){
+    //quiero que en caso de que se click fuera del modal, se cierre
+    if (this.primeraVez){
+      this.primeraVez = false;
+      return;
+    }
+    if (!this.modalRef.nativeElement.contains(event.target)){
+      this.closeModal();
+    }
   }
 
 }
